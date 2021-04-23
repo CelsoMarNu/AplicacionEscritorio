ef56b73770206586a3017116c9fad4cf2ce15db6
Celso Martinez <Celso_MarNu@hotmail.com>
2021-04-23 10:31:30 -0400

    Agregar archivos de proyecto.

---
+++ AplicacionEscritorio/Pedido.Designer.vb
@@ -0,0 +1,335 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Pedido
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GbCabecera = New System.Windows.Forms.GroupBox()
+        Me.GbDetalle = New System.Windows.Forms.GroupBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Codigo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Cantidad = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Precio = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TotalLinea = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.GbCabecera.SuspendLayout()
+        Me.GbDetalle.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GbCabecera
+        '
+        Me.GbCabecera.Controls.Add(Me.Label6)
+        Me.GbCabecera.Controls.Add(Me.Label5)
+        Me.GbCabecera.Controls.Add(Me.Label4)
+        Me.GbCabecera.Controls.Add(Me.Label3)
+        Me.GbCabecera.Controls.Add(Me.Label2)
+        Me.GbCabecera.Controls.Add(Me.Label1)
+        Me.GbCabecera.Controls.Add(Me.TextBox6)
+        Me.GbCabecera.Controls.Add(Me.TextBox5)
+        Me.GbCabecera.Controls.Add(Me.TextBox4)
+        Me.GbCabecera.Controls.Add(Me.TextBox3)
+        Me.GbCabecera.Controls.Add(Me.TextBox2)
+        Me.GbCabecera.Controls.Add(Me.TextBox1)
+        Me.GbCabecera.Location = New System.Drawing.Point(12, 12)
+        Me.GbCabecera.Name = "GbCabecera"
+        Me.GbCabecera.Size = New System.Drawing.Size(457, 203)
+        Me.GbCabecera.TabIndex = 0
+        Me.GbCabecera.TabStop = False
+        Me.GbCabecera.Text = "Cabecera"
+        '
+        'GbDetalle
+        '
+        Me.GbDetalle.Controls.Add(Me.TextBox11)
+        Me.GbDetalle.Controls.Add(Me.TextBox10)
+        Me.GbDetalle.Controls.Add(Me.TextBox9)
+        Me.GbDetalle.Controls.Add(Me.TextBox8)
+        Me.GbDetalle.Controls.Add(Me.TextBox7)
+        Me.GbDetalle.Controls.Add(Me.DataGridView1)
+        Me.GbDetalle.Location = New System.Drawing.Point(12, 221)
+        Me.GbDetalle.Name = "GbDetalle"
+        Me.GbDetalle.Size = New System.Drawing.Size(815, 320)
+        Me.GbDetalle.TabIndex = 1
+        Me.GbDetalle.TabStop = False
+        Me.GbDetalle.Text = "Detalle"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(163, 26)
+        Me.TextBox1.MaxLength = 20
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(87, 27)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(163, 59)
+        Me.TextBox2.MaxLength = 200
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(282, 27)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(163, 92)
+        Me.TextBox3.MaxLength = 10
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(87, 27)
+        Me.TextBox3.TabIndex = 2
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(358, 92)
+        Me.TextBox4.MaxLength = 10
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(87, 27)
+        Me.TextBox4.TabIndex = 3
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(163, 125)
+        Me.TextBox5.MaxLength = 200
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(282, 27)
+        Me.TextBox5.TabIndex = 4
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(163, 158)
+        Me.TextBox6.MaxLength = 10
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(87, 27)
+        Me.TextBox6.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(49, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 20)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Codigo Cliente"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(43, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Nombre Cliente"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(60, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Fecha Pedido"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(256, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Fecha Entrega"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(84, 128)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(73, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Vendedor"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(93, 161)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(64, 20)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Moneda"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Codigo, Me.Descripcion, Me.Cantidad, Me.Precio, Me.TotalLinea})
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 53)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(803, 255)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Codigo
+        '
+        Me.Codigo.HeaderText = "Codigo Articulo"
+        Me.Codigo.MaxInputLength = 20
+        Me.Codigo.MinimumWidth = 6
+        Me.Codigo.Name = "Codigo"
+        Me.Codigo.ReadOnly = True
+        Me.Codigo.Width = 125
+        '
+        'Descripcion
+        '
+        Me.Descripcion.HeaderText = "Descripcion"
+        Me.Descripcion.MaxInputLength = 200
+        Me.Descripcion.MinimumWidth = 6
+        Me.Descripcion.Name = "Descripcion"
+        Me.Descripcion.ReadOnly = True
+        Me.Descripcion.Width = 250
+        '
+        'Cantidad
+        '
+        Me.Cantidad.HeaderText = "Cantidad"
+        Me.Cantidad.MaxInputLength = 18
+        Me.Cantidad.MinimumWidth = 6
+        Me.Cantidad.Name = "Cantidad"
+        Me.Cantidad.ReadOnly = True
+        Me.Cantidad.Width = 125
+        '
+        'Precio
+        '
+        Me.Precio.HeaderText = "Precio"
+        Me.Precio.MaxInputLength = 18
+        Me.Precio.MinimumWidth = 6
+        Me.Precio.Name = "Precio"
+        Me.Precio.ReadOnly = True
+        Me.Precio.Width = 125
+        '
+        'TotalLinea
+        '
+        Me.TotalLinea.HeaderText = "Total"
+        Me.TotalLinea.MaxInputLength = 18
+        Me.TotalLinea.MinimumWidth = 6
+        Me.TotalLinea.Name = "TotalLinea"
+        Me.TotalLinea.ReadOnly = True
+        Me.TotalLinea.Width = 125
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(57, 27)
+        Me.TextBox7.MaxLength = 10
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(125, 27)
+        Me.TextBox7.TabIndex = 6
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(181, 27)
+        Me.TextBox8.MaxLength = 10
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(252, 27)
+        Me.TextBox8.TabIndex = 7
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(432, 27)
+        Me.TextBox9.MaxLength = 10
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(126, 27)
+        Me.TextBox9.TabIndex = 8
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(557, 27)
+        Me.TextBox10.MaxLength = 10
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(126, 27)
+        Me.TextBox10.TabIndex = 7
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(682, 27)
+        Me.TextBox11.MaxLength = 10
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(127, 27)
+        Me.TextBox11.TabIndex = 9
+        '
+        'Pedido
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(832, 553)
+        Me.Controls.Add(Me.GbDetalle)
+        Me.Controls.Add(Me.GbCabecera)
+        Me.Name = "Pedido"
+        Me.Text = "Pedidos"
+        Me.GbCabecera.ResumeLayout(False)
+        Me.GbCabecera.PerformLayout()
+        Me.GbDetalle.ResumeLayout(False)
+        Me.GbDetalle.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GbCabecera As GroupBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GbDetalle As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Codigo As DataGridViewTextBoxColumn
+    Friend WithEvents Descripcion As DataGridViewTextBoxColumn
+    Friend WithEvents Cantidad As DataGridViewTextBoxColumn
+    Friend WithEvents Precio As DataGridViewTextBoxColumn
+    Friend WithEvents TotalLinea As DataGridViewTextBoxColumn
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+End Class
---
+++ AplicacionEscritorio/Pedido.vb
@@ -0,0 +1,11 @@
+﻿
+
+Public Class Pedido
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub GbDetalle_Enter(sender As Object, e As EventArgs) Handles GbDetalle.Enter
+
+    End Sub
+End Class
